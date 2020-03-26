0481d434c477159088fa5ca99eb6d351c7402213
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-03-26 13:55:45 +0100

    Refactors load_dumps.sh to use a single DB

--- db_port/load_dumps.sh
+++ db_port/load_dumps.sh
@@ -13,17 +13,6 @@ echo "  Loading dump for di_sueb_latein"
 mysql -u root -p${rootpasswd} di_sueb_latein < archive/original_dumps/di_sueb_latein.dump
 echo " "
 
-echo "Loading di_sueb_latein into Postgres"
-echo " "
-echo "  Dropping dump_di_sueb_latein"
-psql -c "DROP DATABASE IF EXISTS dump_di_sueb_latein;"
-echo "  Creating dump_di_sueb_latein"
-createdb dump_di_sueb_latein
-echo "  Loading mysql into postgres"
-pgloader mysql://root:${rootpasswd}@localhost/di_sueb_latein postgresql:///dump_di_sueb_latein
-
-
-
 echo "Loading di_sueb_lidos"
 echo " "
 echo "  Dropping di_sueb_lidos"
@@ -33,17 +22,6 @@ mysql -u root -p${rootpasswd} -e "CREATE DATABASE di_sueb_lidos;"
 echo "  Loading dump for di_sueb_lidos"
 mysql -u root -p${rootpasswd}  di_sueb_lidos < archive/original_dumps/di_sueb_lidos.dump
 echo " "
-
-echo "Loading di_sueb_lidos into Postgres"
-echo " "
-echo "  Dropping dump_di_sueb_lidos"
-psql -c "DROP DATABASE IF EXISTS dump_di_sueb_lidos;"
-echo "  Creating dump_di_sueb_lidos"
-createdb dump_di_sueb_lidos
-echo "  Loading mysql into postgres"
-pgloader mysql://root:${rootpasswd}@localhost/di_sueb_lidos postgresql:///dump_di_sueb_lidos
-
-
 
 echo "Loading di_sueb"
 echo " "
@@ -58,20 +36,12 @@ echo " "
 echo "Loading di_sueb into Postgres"
 echo " "
 echo "  Dropping dump_di_sueb"
-psql -c "DROP DATABASE IF EXISTS dump_di_sueb;"
+psql -c "DROP DATABASE IF EXISTS hueb_db_archive;"
 echo "  Creating dump_di_sueb"
-createdb dump_di_sueb
+createdb hueb_db_archive
 echo "  Loading mysql into postgres"
-pgloader mysql://root:${rootpasswd}@localhost/di_sueb postgresql:///dump_di_sueb
+pgloader mysql://root:${rootpasswd}@localhost/di_sueb postgresql:///hueb_db_archive
+pgloader mysql://root:${rootpasswd}@localhost/di_sueb_lidos postgresql:///hueb_db_archive
+pgloader mysql://root:${rootpasswd}@localhost/di_sueb_latein postgresql:///hueb_db_archive
 
 
-echo "Create working copies of databases"
-echo "  Creating di_sueb_latein"
-psql -c "DROP DATABASE IF EXISTS di_sueb_latein;"
-psql -c "CREATE DATABASE di_sueb_latein with TEMPLATE dump_di_sueb_latein;"
-echo "  Creating di_sueb_lidos"
-psql -c "DROP DATABASE IF EXISTS di_sueb_lidos;"
-psql -c "CREATE DATABASE di_sueb_lidos with TEMPLATE dump_di_sueb_lidos;"
-echo "  Creating di_sueb"
-psql -c "DROP DATABASE IF EXISTS di_sueb;"
-psql -c "CREATE DATABASE di_sueb with TEMPLATE dump_di_sueb;"
