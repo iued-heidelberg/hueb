d2b431c90fd2436220ee50284287beb64015c03d
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-03-27 13:58:25 +0100

    Supports search_path in renaming function

--- db_port/sql_scripts/00_setup_postgres.sql
+++ db_port/sql_scripts/00_setup_postgres.sql
@@ -4,11 +4,16 @@ SET search_path to di_sueb_latein;
 CREATE OR REPLACE FUNCTION column_exists(ptable TEXT, pcolumn TEXT)
   RETURNS BOOLEAN AS $BODY$
 DECLARE result bool;
+DECLARE sp TEXT;
 BEGIN
+    -- GET search_path
+    SHOW search_path INTO sp;
+
     -- Does the requested column exist?
     SELECT COUNT(*) INTO result
     FROM information_schema.columns
     WHERE
+      table_schema = sp and
       table_name = ptable and
       column_name = pcolumn;
     RETURN result;
