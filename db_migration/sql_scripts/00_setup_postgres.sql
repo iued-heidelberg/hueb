55c1172f17f7aaaa9ddaef42467dd65b8fabd56b
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-04-10 23:23:19 +0200

    Adds functions to simplify creation of fk-constraints

--- db_migration/sql_scripts/00_setup_postgres.sql
+++ db_migration/sql_scripts/00_setup_postgres.sql
@@ -31,3 +31,77 @@ BEGIN
 END$BODY$
   LANGUAGE plpgsql VOLATILE;
 
+CREATE OR REPLACE FUNCTION dangling_references_exists(primary_table TEXT, primary_column TEXT, secondary_table TEXT)
+  RETURNS BOOLEAN AS $BODY$
+DECLARE result bool;
+
+BEGIN
+    EXECUTE FORMAT('
+      SELECT EXISTS(
+        SELECT %I
+        FROM %I
+        WHERE %I NOT IN (
+          SELECT id
+          FROM %I
+        )
+      );',
+    primary_column, primary_table, primary_column, secondary_table)
+    INTO result;
+    RETURN result;
+  END$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE FUNCTION clean_up_relation(primary_table TEXT, primary_column TEXT, secondary_table TEXT)
+  RETURNS BOOLEAN AS $BODY$
+DECLARE result bool;
+DECLARE placeholder BIGINT;
+BEGIN
+
+  -- drop primary key
+    EXECUTE FORMAT('
+      ALTER TABLE %s
+      DROP CONSTRAINT IF EXISTS %I;',
+      primary_table, secondary_table||'_fk');
+
+
+  -- check if a placeholder is necessary
+    IF dangling_references_exists(primary_table, primary_column, secondary_table)
+    THEN
+      -- insert placeholder
+        EXECUTE FORMAT('
+          INSERT INTO %I(migration_notes, migration_generated)
+          VALUES ($$Automatisch als Platzhalter angelegt$$, true)
+          RETURNING id;',
+          secondary_table)
+        INTO placeholder;
+
+        -- move dangling references to placeholder
+        EXECUTE FORMAT('
+        UPDATE %I
+        SET %s = %s
+        WHERE id IN(
+          SELECT %s
+          FROM %s
+          WHERE %s NOT IN (
+            SELECT id
+            FROM %s
+          )
+        )
+        RETURNING *;',
+        primary_table, primary_column, placeholder, primary_table||'.id', primary_table, primary_table || '.' || primary_column, secondary_table);
+
+
+    END IF;
+
+  -- create primary key
+    EXECUTE FORMAT('
+       alter table %s
+        add constraint %I
+          foreign key (%s) references "%s"
+            on update cascade on delete restrict;',
+            primary_table, secondary_table||'_fk', primary_column, secondary_table);
+
+
+    RETURN placeholder;
+  END$BODY$
+  LANGUAGE plpgsql VOLATILE;
