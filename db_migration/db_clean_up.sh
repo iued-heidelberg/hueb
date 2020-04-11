de76bd88fea2e2da9b5f81c2e0686c158bd5bc1c
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-04-11 14:18:41 +0200

    Implements m:n tables for original and translation
    This relates to #14

--- db_migration/db_clean_up.sh
+++ db_migration/db_clean_up.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -e
+echo "Create working copies of databases"
+echo "  Creating hueb_db"
+psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
+        FROM pg_stat_activity
+        WHERE pg_stat_activity.datname = 'hueb_db'
+          AND pid <> pg_backend_pid();"
+psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
+        FROM pg_stat_activity
+        WHERE pg_stat_activity.datname = 'hueb_db_archive'
+          AND pid <> pg_backend_pid();"
+psql -c "DROP DATABASE IF EXISTS hueb_db;"
+psql -c "CREATE DATABASE hueb_db with TEMPLATE hueb_db_archive;"
 
 psql -d hueb_db -a -f sql_scripts/00_setup_postgres.sql
 psql -d hueb_db -a -f sql_scripts/01_table_cleanup.sql
 psql -d hueb_db -a -f sql_scripts/02_add_migration_notes.sql
-psql -d hueb_db -a -f sql_scripts/03_add_foreign_key_constraints.sql
+psql -d hueb_db -a -f sql_scripts/02_create_n_m_tables.sql
+#psql -d hueb_db -a -f sql_scripts/03_add_foreign_key_constraints.sql
