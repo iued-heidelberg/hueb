e1f746cab99696ec3aae95b630a142db9dd5d3d4
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-04-11 14:49:47 +0200

    Changes method for relation cleanup
    Closes #13

--- db_migration/db_clean_up.sh
+++ db_migration/db_clean_up.sh
@@ -17,4 +17,4 @@ psql -d hueb_db -a -f sql_scripts/00_setup_postgres.sql
 psql -d hueb_db -a -f sql_scripts/01_table_cleanup.sql
 psql -d hueb_db -a -f sql_scripts/02_add_migration_notes.sql
 psql -d hueb_db -a -f sql_scripts/02_create_n_m_tables.sql
-#psql -d hueb_db -a -f sql_scripts/03_add_foreign_key_constraints.sql
+psql -d hueb_db -a -f sql_scripts/03_add_foreign_key_constraints.sql
