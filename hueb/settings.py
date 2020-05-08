a07554409d5cb940be6a7b5a8f42dde641f68a93
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-05-08 23:29:33 +0200

    Adds allowed_hosts setting

--- hueb/settings.py
+++ hueb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("HUEB_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("HUEB_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("HUEB_ALLOWED_HOSTS")]
 
 
 # Application definition
