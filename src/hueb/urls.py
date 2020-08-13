aa6327e9b5fd5647cd890c46fbc7ab20bcf5cbd4
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-08-13 22:37:11 +0200

    Adds the debug toolbar

--- src/hueb/urls.py
+++ src/hueb/urls.py
@@ -13,8 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+DEBUG = os.getenv("HUEB_DEBUG") == "TRUE"
 
 
 def trigger_error(request):
@@ -24,4 +30,9 @@ def trigger_error(request):
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("sentry-debug/", trigger_error),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
