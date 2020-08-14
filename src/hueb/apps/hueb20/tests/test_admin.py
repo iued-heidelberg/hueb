2e3a8907631709cb61c2087fd1392ae349aa5759
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-08-15 00:16:29 +0200

    Fixes tests

--- src/hueb/apps/hueb20/tests/test_admin.py
+++ src/hueb/apps/hueb20/tests/test_admin.py
@@ -1,27 +1,43 @@
-import inspect
-
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as DjangoUser
 from django.test import Client, TestCase
 from django.urls import reverse
-from hueb.apps.hueb20 import models
+
+from hueb.apps.hueb20.models import (  # OriginalAuthorNew,; OriginalNewAuthor,; TranslationNewTranslator,; TranslationTranslatorNew,
+    Archive,
+    Country,
+    DdcGerman,
+    Document,
+    Language,
+    Location,
+    Person,
+    YearRange,
+)
 
 
-class AdminTestCase(TestCase):
+class AdminSmokeTestCase(TestCase):
+    admin_sites = [
+        Archive,
+        Country,
+        DdcGerman,
+        Document,
+        Language,
+        Location,
+        Person,
+        YearRange,
+    ]
+
     def setUp(self):
         self.client = Client()
-        user = User.objects.create_superuser(username="test", password="test",)
+        user = DjangoUser.objects.create_superuser(username="test", password="test",)
         self.client.force_login(user)
 
     def test_smoke_admin(self):
-        for name, obj in inspect.getmembers(models):
-            if inspect.isclass(obj):
-                if obj.__module__ == models.__name__:
-                    instance = obj()
-                    instance.save()
-                    info = (instance._meta.app_label, instance._meta.model_name)
-                    admin_url = reverse(
-                        "admin:%s_%s_change" % info, args=(instance.pk,)
-                    )
+        for obj in self.admin_sites:
+            instance = obj()
+            instance.save()
 
-                    response = self.client.get(admin_url)
-                    self.assertEqual(response.status_code, 200)
+            info = (instance._meta.app_label, instance._meta.model_name)
+            admin_url = reverse("admin:%s_%s_change" % info, args=(instance.pk,))
+
+            response = self.client.get(admin_url)
+            self.assertEqual(response.status_code, 200)
