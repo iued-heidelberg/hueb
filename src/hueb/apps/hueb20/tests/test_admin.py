a6ca09177447bed8866bb11fa0d16fe481a27430
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 11:22:15 +0200

    Switches tests to pytest and pytest_django

--- src/hueb/apps/hueb20/tests/test_admin.py
+++ src/hueb/apps/hueb20/tests/test_admin.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import User as DjangoUser
-from django.test import Client, TestCase
+import pytest
 from django.urls import reverse
 from hueb.apps.hueb20.models.archive import Archive
 from hueb.apps.hueb20.models.comment import Comment
@@ -11,32 +10,26 @@ from hueb.apps.hueb20.models.filing import Filing
 from hueb.apps.hueb20.models.language import Language
 from hueb.apps.hueb20.models.person import Person
 
+admin_sites = [
+    Filing,
+    Country,
+    DdcGerman,
+    Document,
+    Language,
+    Archive,
+    Person,
+    Comment,
+    CulturalCircle,
+]
 
-class AdminSmokeTestCase(TestCase):
-    admin_sites = [
-        Filing,
-        Country,
-        DdcGerman,
-        Document,
-        Language,
-        Archive,
-        Person,
-        Comment,
-        CulturalCircle,
-    ]
 
-    def setUp(self):
-        self.client = Client()
-        user = DjangoUser.objects.create_superuser(username="test", password="test",)
-        self.client.force_login(user)
+@pytest.mark.django_db
+@pytest.mark.parametrize("site", admin_sites)
+def test_smoke_admin(admin_client, site):
+    instance = site()
+    instance.save()
+    info = (site._meta.app_label, site._meta.model_name)
+    admin_url = reverse("admin:%s_%s_change" % info, args=(instance.pk,))
 
-    def test_smoke_admin(self):
-        for obj in self.admin_sites:
-            instance = obj()
-            instance.save()
-
-            info = (instance._meta.app_label, instance._meta.model_name)
-            admin_url = reverse("admin:%s_%s_change" % info, args=(instance.pk,))
-
-            response = self.client.get(admin_url)
-            self.assertEqual(response.status_code, 200)
+    response = admin_client.get(admin_url)
+    assert response.status_code == 200
