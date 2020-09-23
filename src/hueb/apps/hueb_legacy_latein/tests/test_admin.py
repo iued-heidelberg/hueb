c669a19b0d45aae132794ee89d47788df807e83f
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 11:32:28 +0200

    Adds search smoke tests for hueb_legacy_latein

--- src/hueb/apps/hueb_legacy_latein/tests/test_admin.py
+++ src/hueb/apps/hueb_legacy_latein/tests/test_admin.py
@@ -41,3 +41,14 @@ def test_smoke_admin(admin_client, site):
 
     response = admin_client.get(admin_url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("site", admin_sites)
+def test_smoke_admin_search(admin_client, site):
+    info = (site._meta.app_label, site._meta.model_name)
+    admin_url = reverse("admin:%s_%s_changelist" % info)
+    print(admin_url)
+    search_url = admin_url + "?q=test"
+    response = admin_client.get(search_url)
+    assert response.status_code == 200
