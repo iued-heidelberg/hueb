3865e26a8669c47c6414f160261dc6c171fcadda
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 16:30:17 +0200

    redirects to admin on successfull login

--- src/hueb/settings.py
+++ src/hueb/settings.py
@@ -23,7 +23,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hueb.settings")
 
 ENV = os.environ.get("ENV", None)
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/admin"
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
