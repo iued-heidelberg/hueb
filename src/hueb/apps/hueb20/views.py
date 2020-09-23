a4d85a7556adcf16b19ad14c7d45483244119cc5
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 15:26:20 +0200

    Extracts common context

--- src/hueb/apps/hueb20/views.py
+++ src/hueb/apps/hueb20/views.py
@@ -2,17 +2,21 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
 
-def index(request):
-
+def get_common_context(context={}):
     menu = []
     menu.append({"name": "Projekt", "link": "#", "disabled": False})
     menu.append({"name": "Katalog", "link": "#", "disabled": True})
     menu.append({"name": "Suche", "link": "#", "disabled": True})
+    context["menu"] = menu
 
-    context = {
-        "menu": menu,
-        "form": AuthenticationForm(initial={"username": "", "password": ""}),
-        "overlayOpen": True,
-    }
+    context["overlayOpen"] = False
+
+    return context
+
+
+def index(request):
+
+    context = get_common_context()
+    context["form"] = AuthenticationForm(initial={"username": "", "password": ""})
 
     return render(request, "hueb20/index.html", context)
