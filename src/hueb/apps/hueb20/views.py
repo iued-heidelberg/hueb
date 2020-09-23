232615a3e7c9795f72f572d27f13ad5c397da9f7
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 16:59:50 +0200

    Fixes initial value problem

--- src/hueb/apps/hueb20/views.py
+++ src/hueb/apps/hueb20/views.py
@@ -1,1 +1,55 @@
-# Create your views here.
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.forms.widgets import PasswordInput, TextInput
+from django.shortcuts import render
+
+
+def get_common_context(context={}):
+    menu = []
+    menu.append({"name": "Projekt", "link": "#", "disabled": False})
+    menu.append({"name": "Katalog", "link": "#", "disabled": True})
+    menu.append({"name": "Suche", "link": "#", "disabled": True})
+    context["menu"] = menu
+
+    context["overlayOpen"] = False
+
+    return context
+
+
+class CustomAuthForm(AuthenticationForm):
+    username = forms.CharField(
+        initial="",
+        widget=TextInput(attrs={"class": "validate", "placeholder": "Benutzername"}),
+    )
+    password = forms.CharField(
+        initial="", widget=PasswordInput(attrs={"placeholder": "Passwort"})
+    )
+
+
+def index(request):
+    context = get_common_context()
+    context["form"] = CustomAuthForm(initial={"username": "", "password": ""})
+
+    return render(request, "hueb20/index.html", context)
+
+
+class Login(LoginView):
+    template_name = "hueb20/index.html"
+    redirect_authenticated_user = True
+    authentication_form = CustomAuthForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = get_common_context(context)
+        context["overlayOpen"] = True
+        return context
+
+
+class Logout(LogoutView):
+    template_name = "hueb20/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = get_common_context(context)
+        return context
