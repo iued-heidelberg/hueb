a92b421068b0280549553d090b189a02a6c1418c
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-09-23 16:29:50 +0200

    Adds logout view

--- src/hueb/apps/hueb20/views.py
+++ src/hueb/apps/hueb20/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
 
@@ -16,7 +16,6 @@ def get_common_context(context={}):
 
 
 def index(request):
-
     context = get_common_context()
     context["form"] = AuthenticationForm(initial={"username": "", "password": ""})
 
@@ -31,3 +30,13 @@ class Login(LoginView):
         context = super().get_context_data(**kwargs)
         context = get_common_context(context)
         context["overlayOpen"] = True
+        return context
+
+
+class Logout(LogoutView):
+    template_name = "hueb20/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = get_common_context(context)
+        return context
