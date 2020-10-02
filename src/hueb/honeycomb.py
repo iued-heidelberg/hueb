6fa62e1f249ebe4edd9f71c71ea2d1907038d927
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-10-02 17:06:27 +0200

    Adds release markes to honeycomb

--- src/hueb/honeycomb.py
+++ src/hueb/honeycomb.py
@@ -4,13 +4,14 @@ import beeline
 
 
 def honeycomb_config():
-    return beeline.init(
-        writekey=os.getenv("HUEB_HONEYCOMB_API_KEY"),
-        dataset="hueb_" + os.getenv("HUEB_ENV"),
-        service_name="hueb",
-        debug=False,
-        presend_hook=presend,
-    )
+    if not os.getenv("ENV") == "GITHUB_WORKFLOW":
+        return beeline.init(
+            writekey=os.getenv("HUEB_HONEYCOMB_API_KEY"),
+            dataset="hueb_" + os.getenv("HUEB_ENV"),
+            service_name="hueb",
+            debug=False,
+            presend_hook=presend,
+        )
 
 
 def presend(fields):
