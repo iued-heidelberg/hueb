39c8a6bcc52caf78a1685213efcfc7dcdb48aad7
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-10-17 15:17:33 +0200

    Adds tailwind build-chain

---
+++ src/hueb/apps/hueb20/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [
+    './templates/hueb20/**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'seal-fg': '#c61826',
+        'seal-bg': '#590d08',
+        'sand-bg': '#f4f1ea',
+        'sand-100': "#fbfbf8",
+        'sand-200': "#f4f1ea",
+        'sand-300': "#ede9de",
+        'sand-400': "#e5ddcd",
+        'sand-500': "#dcd2bc",
+        'sand-600': "#d3c7ab",
+        'sand-700': "#d1c5a8",
+        'sand-800': "#c8ba97",
+        'sand-900': "#c0af87",
+        'gray-bg-light': '#f2f5f6',
+        'gray-bg': '#222222',
+        'gray-bg-darker': '#171717',
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+  ],
+}
