58cb8f89bbbb98b80759f25abefef3a3a664678e
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-10-17 18:24:35 +0200

    Moves menu generation into context_processor

---
+++ src/hueb/apps/hueb20/context_processors.py
@@ -0,0 +1,16 @@
+def menu(request):
+    menu = []
+
+    menu.append({"name": "Projekt", "link": "/", "disabled": False})
+
+    if request.user.is_authenticated:
+        menu.append({"name": "Suche", "link": "/search", "disabled": False})
+    else:
+        menu.append({"name": "Suche", "link": "#", "disabled": True})
+
+    menu.append({"name": "Katalog", "link": "#", "disabled": True})
+
+    context = {}
+    context["menu"] = menu
+
+    return context
