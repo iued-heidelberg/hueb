54e21b889029f5e1b16da0ac9d140691bea15f43
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-10-18 23:18:18 +0200

    Adds simple search form

--- src/hueb/apps/hueb20/urls.py
+++ src/hueb/apps/hueb20/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from hueb.apps.hueb20.views.authentication import Login, Logout
 from hueb.apps.hueb20.views.index import IndexView
-from hueb.apps.hueb20.views.search import search
+from hueb.apps.hueb20.views.search import Search
 
 urlpatterns = [
     # ex: /polls/
     path("", IndexView.as_view(), name="Index"),
-    path("search", search, name="Search"),
+    path("search", Search.as_view(), name="search"),
     path("accounts/login", Login.as_view(), name="login"),
     path("accounts/logout", Logout.as_view(), name="logout"),
 ]
--- src/hueb/apps/hueb20/views/search.py
+++ src/hueb/apps/hueb20/views/search.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.generic import ListView
+from hueb.apps.hueb20.models.document import Document
 
 
 @login_required
 def search(request):
-    return render(request, "hueb20/search.html")
+    return render(request, "hueb20/search/search.html")
+
+
+class Search(ListView):
+    model = Document
+    template_name = "hueb20/search/search.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_val = self.request.GET.get("filter")
+        if filter_val is not None:
+            new_context = Document.objects.filter(title__icontains=filter_val)
+        else:
+            new_context = Document.objects.all()
+        return new_context
+
+    def get_context_data(self, **kwargs):
+        context = super(Search, self).get_context_data(**kwargs)
+        context["filter"] = self.request.GET.get("filter", "")
+        return context
