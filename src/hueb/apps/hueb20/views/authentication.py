49200b00fa061ee836d96094d89d69a684cb9ee6
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-10-28 18:05:48 +0100

    Removes unneeded customForm

--- src/hueb/apps/hueb20/views/authentication.py
+++ src/hueb/apps/hueb20/views/authentication.py
@@ -1,31 +1,11 @@
-from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.forms.widgets import PasswordInput, TextInput
-
-
-class CustomAuthForm(AuthenticationForm):
-    username = forms.CharField(
-        widget=TextInput(
-            attrs={
-                "class": "validate",
-                "placeholder": "Benutzername",
-                "autofocus": True,
-            }
-        ),
-    )
-    password = forms.CharField(
-        strip=False,
-        widget=PasswordInput(
-            attrs={"placeholder": "Passwort", "autocomplete": "current-password"}
-        ),
-    )
 
 
 class Login(LoginView):
     template_name = "hueb20/index.html"
     redirect_authenticated_user = True
-    authentication_form = CustomAuthForm
+    authentication_form = AuthenticationForm
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
