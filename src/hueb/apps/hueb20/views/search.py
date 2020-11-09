cb6bed7251aefbdae83e385cbff3b5169758248a
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-09 11:42:40 +0100

    Drops simple search

--- src/hueb/apps/hueb20/views/search.py
+++ src/hueb/apps/hueb20/views/search.py
@@ -110,8 +110,8 @@ class BaseSearchFormSet(BaseFormSet):
             return queryset
 
 
-class FormsetSearch(ListView):
-    template_name = "hueb20/search/search_complex.html"
+class Search(ListView):
+    template_name = "hueb20/search/search.html"
     model = Document
     paginate_by = 20
 
@@ -137,7 +137,7 @@ class FormsetSearch(ListView):
             return results
 
     def get_context_data(self, **kwargs):
-        context = super(FormsetSearch, self).get_context_data(**kwargs)
+        context = super(Search, self).get_context_data(**kwargs)
 
         try:
             formset = self.SearchFormset(data=self.request.GET)
@@ -147,52 +147,4 @@ class FormsetSearch(ListView):
 
         context["formset"] = formset
 
-        return context
-
-
-class Search(ListView):
-    model = Document
-    template_name = "hueb20/search/search.html"
-    paginate_by = 20
-
-    def get_queryset(self):
-        filter_val = self.request.GET.get("filter")
-        if filter_val is not None:
-            results = (
-                Document.objects.prefetch_related("translations__written_by")
-                .prefetch_related("translations__ddc")
-                .prefetch_related("translations__language")
-                .prefetch_related("originals__written_by")
-                .prefetch_related("originals__ddc")
-                .prefetch_related("originals__language")
-                .prefetch_related("written_by")
-                .select_related("language")
-                .select_related("ddc")
-                .filter(
-                    Q(title__icontains=filter_val)
-                    | Q(written_by__name__icontains=filter_val)
-                    | Q(language__language__icontains=filter_val)
-                )
-                .order_by("id")
-            )
-            return results
-        else:
-            result = (
-                Document.objects.prefetch_related("translations__written_by")
-                .prefetch_related("translations__ddc")
-                .prefetch_related("translations__language")
-                .prefetch_related("originals__written_by")
-                .prefetch_related("originals__ddc")
-                .prefetch_related("originals__language")
-                .prefetch_related("written_by")
-                .select_related("language")
-                .select_related("ddc")
-                .all()
-                .order_by("id")
-            )
-        return result
-
-    def get_context_data(self, **kwargs):
-        context = super(Search, self).get_context_data(**kwargs)
-        context["filter"] = self.request.GET.get("filter", "")
         return context
