d94694468e24dda2d1e020b9df902b3eb1f8962f
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-09 23:34:11 +0100

    adds changelist test to the admin smoke tests

--- src/hueb/apps/hueb20/tests/test_admin.py
+++ src/hueb/apps/hueb20/tests/test_admin.py
@@ -25,22 +25,18 @@ admin_sites = [
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("site", admin_sites)
-def test_smoke_admin(admin_client, site):
-    instance = site()
-    instance.save()
-    info = (site._meta.app_label, site._meta.model_name)
-    admin_url = reverse("admin:%s_%s_change" % info, args=(instance.pk,))
-
-    response = admin_client.get(admin_url)
-    assert response.status_code == 200
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize("site", admin_sites)
 def test_smoke_admin_search(admin_client, site):
     info = (site._meta.app_label, site._meta.model_name)
     admin_url = reverse("admin:%s_%s_changelist" % info)
-    print(admin_url)
+    response = admin_client.get(admin_url)
+    assert response.status_code == 200
+
     search_url = admin_url + "?q=test"
     response = admin_client.get(search_url)
+    assert response.status_code == 200
+
+    instance = site()
+    instance.save()
+    instance_url = reverse("admin:%s_%s_change" % info, args=(instance.pk,))
+    response = admin_client.get(instance_url)
     assert response.status_code == 200
