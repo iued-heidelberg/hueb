e3ddc6df88cce5531492e2bbdda66e6c8ff69c5a
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-11-10 17:33:05 +0100

    moves urls to hueb20

--- src/hueb/apps/hueb20/urls.py
+++ src/hueb/apps/hueb20/urls.py
@@ -1,12 +1,21 @@
-from django.urls import path
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from hueb import settings
 from hueb.apps.hueb20.views.authentication import Login, Logout
 from hueb.apps.hueb20.views.index import IndexView
 from hueb.apps.hueb20.views.search import Search
 
 urlpatterns = [
     # ex: /polls/
+    path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="Index"),
     path("search", Search.as_view(), name="search"),
     path("accounts/login", Login.as_view(), name="login"),
     path("accounts/logout", Logout.as_view(), name="logout"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
--- src/hueb/urls.py
+++ src/hueb/urls.py
@@ -13,27 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
+
 from django.urls import include, path
-
-DEBUG = os.getenv("HUEB_DEBUG") == "TRUE"
-
-
-def trigger_error(request):
-    print(1 / 0)
-
 
 urlpatterns = [
     path("", include("hueb.apps.hueb20.urls")),
-    path("admin/", admin.site.urls),
-    path("sentry-debug/", trigger_error),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
+]
