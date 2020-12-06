fdf9bebc4720921e8d3c6e34842f8fe322a5c780
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-12-06 23:20:25 +0100

    changes backup mechanism

--- deployment/docker/postgres/pg_backup.sh
+++ deployment/docker/postgres/pg_backup.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+set -e
 file_name=postgres_dump
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $current_time"
 
-new_fileName=/backup/$file_name.$current_time.gz
+new_fileName=$1/$file_name.$current_time.gz
 echo "New FileName: " "$new_fileName"
 
 touch $new_fileName
 
 echo "Dumping: " "$POSTGRES_DB"
 
-pg_dump -h $1 -U $POSTGRES_USER $POSTGRES_DB | gzip > $new_fileName
+pg_dump hueb | gzip > $new_fileName
