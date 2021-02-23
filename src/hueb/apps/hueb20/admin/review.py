0699b3417b076624e040667bbe52efd99958cec8
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-02-23 15:09:02 +0100

    fixes faulty non reviewer permissions

--- src/hueb/apps/hueb20/admin/review.py
+++ src/hueb/apps/hueb20/admin/review.py
@@ -18,9 +18,9 @@ class ReviewAdmin(SimpleHistoryAdmin):
 class TabularInlineReviewAdmin(admin.TabularInline):
     def get_readonly_fields(self, request, obj=None):
         if not request.user.has_perm("hueb20.can_review"):
-            return (
+            return self.readonly_fields + (
                 "state",
                 "reviewed",
-            ).update(self.readonly_fields)
+            )
         else:
             return self.readonly_fields
