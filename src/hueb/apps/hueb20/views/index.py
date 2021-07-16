10dbf7847d633b5eb7c0b88b17864c409adb731b
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-07-17 00:19:08 +0200

    Fixes double login bug

--- src/hueb/apps/hueb20/views/index.py
+++ src/hueb/apps/hueb20/views/index.py
@@ -1,9 +1,10 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 
-class IndexView(TemplateView):
+class IndexView(LoginView):
     template_name = "hueb20/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["overlayOpen"] = False
         return context
