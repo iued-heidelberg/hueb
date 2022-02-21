6355ff1909fa79d4602aabcde46fdb16bede2f29
annadymanus <aniadymanus@gmail.com>
2022-02-21 19:19:38 +0100

    Black 0064_update_no_pairs.py

--- src/hueb/apps/hueb20/migrations/0064_update_no_pairs.py
+++ src/hueb/apps/hueb20/migrations/0064_update_no_pairs.py
@@ -5,11 +5,17 @@ import re
 
 def update_latein(apps, schema_editor):
     DocumentRelationship = apps.get_model("hueb20", "DocumentRelationship")
-    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(document_from__title__isnull=True).all().delete()
-    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(document_to__title__isnull=True).all().delete()
+    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(
+        document_from__title__isnull=True
+    ).all().delete()
+    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(
+        document_to__title__isnull=True
+    ).all().delete()
+
 
 def undo_latein(apps, schema_editor):
     pass
+
 
 class Migration(migrations.Migration):
 
@@ -17,6 +23,4 @@ class Migration(migrations.Migration):
         ("hueb20", "0062_auto_20220212_1534"),
     ]
 
-    operations = [
-        migrations.RunPython(update_latein, undo_latein)
-    ]
+    operations = [migrations.RunPython(update_latein, undo_latein)]
