52419e96b3ac77cfec4d9f8a79fdb610269ca7fe
annadymanus <aniadymanus@gmail.com>
2022-02-21 19:16:25 +0100

    Migration - Remove links to empty document

---
+++ src/hueb/apps/hueb20/migrations/0064_update_no_pairs.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from hueb.apps.hueb20.models import LATEIN
+import re
+
+
+def update_latein(apps, schema_editor):
+    DocumentRelationship = apps.get_model("hueb20", "DocumentRelationship")
+    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(document_from__title__isnull=True).all().delete()
+    DocumentRelationship.objects.filter(document_from__app=LATEIN).filter(document_to__title__isnull=True).all().delete()
+
+def undo_latein(apps, schema_editor):
+    pass
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("hueb20", "0062_auto_20220212_1534"),
+    ]
+
+    operations = [
+        migrations.RunPython(update_latein, undo_latein)
+    ]
