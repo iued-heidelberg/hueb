d27263acae92d53f0faf968b19e69497c8239d18
annadymanus <aniadymanus@gmail.com>
2024-07-16 19:19:08 +0200

    fix login redirect

--- src/hueb/apps/hueb20/views/authentication.py
+++ src/hueb/apps/hueb20/views/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import resolve_url
+from django.conf import settings
 
 
 class Login(LoginView):
@@ -11,6 +13,14 @@ class Login(LoginView):
         context = super().get_context_data(**kwargs)
         context["overlayOpen"] = True
         return context
+
+    def get_success_url(self):
+        # check if user has staff status
+        if self.request.user.is_staff:
+            return super().get_success_url()
+        else:
+            # redirect to index page
+            return resolve_url("Index")
 
 
 class Logout(LogoutView):
