91fa57f046772eef13e971a017bf5b224d33c829
annadymanus <aniadymanus@gmail.com>
2024-07-27 22:34:09 +0200

    undo authentification changes

--- src/hueb/apps/hueb20/views/authentication.py
+++ src/hueb/apps/hueb20/views/authentication.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import resolve_url
-from django.conf import settings
+
+# from django.shortcuts import resolve_url
 
 
 class Login(LoginView):
@@ -14,6 +14,8 @@ class Login(LoginView):
         context["overlayOpen"] = True
         return context
 
+
+"""
     def get_success_url(self):
         # check if user has staff status
         if self.request.user.is_staff:
@@ -21,6 +23,7 @@ class Login(LoginView):
         else:
             # redirect to index page
             return resolve_url("Index")
+"""
 
 
 class Logout(LogoutView):
